fcb67b7206a5f3204c1781acc83f3226ea50c1cb
Yamlak Negash <yamlaknegash96@gmail.com>
2026-04-20 08:05:16 +0300

    import corrected

--- Frontend/src/components/ui/input-group.tsx
+++ Frontend/src/components/ui/input-group.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
-import { Button } from "./button"
+import { Button } from "./Button"
 import { Input } from "./input"
 import { Textarea } from "./textarea"
 
